dec7fd9e2425e2f4a3f7412238ceee22923f32f8
Sasaki Keisuke <ksk.andr@gmail.com>
2025-09-28 23:40:16 +0900

    日本の祝日カレンダーのデータを追加

---
+++ events.js
@@ -0,0 +1,163 @@
+const calendarEvents = [
+  {
+    title: '元日',
+    start: '2025-01-01',
+    allDay: true,
+  },
+  {
+    title: '成人の日',
+    start: '2025-01-13',
+    allDay: true,
+  },
+  {
+    title: '建国記念の日',
+    start: '2025-02-11',
+    allDay: true,
+  },
+  {
+    title: '天皇誕生日',
+    start: '2025-02-23',
+    allDay: true,
+  },
+  {
+    title: '春分の日',
+    start: '2025-03-20',
+    allDay: true,
+  },
+  {
+    title: '昭和の日',
+    start: '2025-04-29',
+    allDay: true,
+  },
+  {
+    title: '憲法記念日',
+    start: '2025-05-03',
+    allDay: true,
+  },
+  {
+    title: 'みどりの日',
+    start: '2025-05-04',
+    allDay: true,
+  },
+  {
+    title: 'こどもの日',
+    start: '2025-05-05',
+    allDay: true,
+  },
+  {
+    title: '海の日',
+    start: '2025-07-21',
+    allDay: true,
+  },
+  {
+    title: '山の日',
+    start: '2025-08-11',
+    allDay: true,
+  },
+  {
+    title: '敬老の日',
+    start: '2025-09-15',
+    allDay: true,
+  },
+  {
+    title: '秋分の日',
+    start: '2025-09-23',
+    allDay: true,
+  },
+  {
+    title: 'スポーツの日',
+    start: '2025-10-13',
+    allDay: true,
+  },
+  {
+    title: '文化の日',
+    start: '2025-11-03',
+    allDay: true,
+  },
+  {
+    title: '勤労感謝の日',
+    start: '2025-11-23',
+    allDay: true,
+  },
+  // 2026年の祝日
+  {
+    title: '元日',
+    start: '2026-01-01',
+    allDay: true,
+  },
+  {
+    title: '成人の日',
+    start: '2026-01-12',
+    allDay: true,
+  },
+  {
+    title: '建国記念の日',
+    start: '2026-02-11',
+    allDay: true,
+  },
+  {
+    title: '天皇誕生日',
+    start: '2026-02-23',
+    allDay: true,
+  },
+  {
+    title: '春分の日',
+    start: '2026-03-20',
+    allDay: true,
+  },
+  {
+    title: '昭和の日',
+    start: '2026-04-29',
+    allDay: true,
+  },
+  {
+    title: '憲法記念日',
+    start: '2026-05-03',
+    allDay: true,
+  },
+  {
+    title: 'みどりの日',
+    start: '2026-05-04',
+    allDay: true,
+  },
+  {
+    title: 'こどもの日',
+    start: '2026-05-05',
+    allDay: true,
+  },
+  {
+    title: '海の日',
+    start: '2026-07-20',
+    allDay: true,
+  },
+  {
+    title: '山の日',
+    start: '2026-08-11',
+    allDay: true,
+  },
+  {
+    title: '敬老の日',
+    start: '2026-09-21',
+    allDay: true,
+  },
+  {
+    title: '秋分の日',
+    start: '2026-09-23',
+    allDay: true,
+  },
+  {
+    title: 'スポーツの日',
+    start: '2026-10-12',
+    allDay: true,
+  },
+  {
+    title: '文化の日',
+    start: '2026-11-03',
+    allDay: true,
+  },
+  {
+    title: '勤労感謝の日',
+    start: '2026-11-23',
+    allDay: true,
+  },
+];
